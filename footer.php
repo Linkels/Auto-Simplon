e8026e37bdbebd97dab812226adfba163418b212
Linkels <48438463+Linkels@users.noreply.github.com>
2019-03-29 10:24:31 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<link href="css/footer.css" rel="stylesheet" type="text/css">
+      <footer class="p-2">
+        <div class="row">
+          <div class="col-sm-2">
+            <div id="logo">
+            <a href="index.php">
+            <img src="img/logo.png" width=55/>
+            </div>
+          </div>
+          <div class="tx-a-d col-sm-4">
+            <a href="https://www.facebook.com/Simplon.co/?fref=nf">
+            <img src="img/facebook.png" width=35/></a>
+            <a href="https://twitter.com/simplonco">
+            <img src="img/twitter.png" width=35/></a>
+            <a href="mailto:autosimplon@autosimplon.co">
+            <img src="img/mail.png" width=35/></a>
+          </div>
+          <div class="tx-a-d col-sm-6">
+            <a class="text-white" href="contact.php">Nous Contacter</a><br/>
+            <a class="text-white" href="mention.php">Mentions Légales</a>
+          </div>
+        </div>
+      </footer>
